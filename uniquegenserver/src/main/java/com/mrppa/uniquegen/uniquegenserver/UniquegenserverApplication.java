d06bd81788a5121eb70bd2b7a7ac7b6bc203c608
Pasindu <mail2ppa@gmail.com>
2022-12-31 00:58:01 +0530

    bootstrap standalone module

---
+++ uniquegenserver/src/main/java/com/mrppa/uniquegen/uniquegenserver/UniquegenserverApplication.java
@@ -0,0 +1,13 @@
+package com.mrppa.uniquegen.uniquegenserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniquegenserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniquegenserverApplication.class, args);
+	}
+
+}
---
+++ uniquegenserver/src/test/java/com/mrppa/uniquegen/uniquegenserver/UniquegenserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrppa.uniquegen.uniquegenserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniquegenserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
